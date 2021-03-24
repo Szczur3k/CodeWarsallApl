568345707e24b47098a2cc3985f3b09b0a159be1
Szczur3k <mateuszlukaszczyk13@gmail.com>
2021-03-24 09:34:00 +0100

    Commit camelCase

--- src/main/java/BreakCamelCase/CamelCase.java
+++ src/main/java/BreakCamelCase/CamelCase.java
@@ -1,2 +1,19 @@
-package BreakCamelCase;public class CamelCase {
+package BreakCamelCase;
+
+public class CamelCase {
+
+    public static String breakCamelCase(String input) {
+        String[] splittedInput = input.split("");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String split : splittedInput) {
+            if(split.equals(split.toUpperCase()) && stringBuilder.length()!=0){
+                stringBuilder.append(" ").append(split);
+            } else {
+                stringBuilder.append(split);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
 }
--- src/test/java/BreakCamelCase/CamelCaseTest.java
+++ src/test/java/BreakCamelCase/CamelCaseTest.java
@@ -1,4 +1,31 @@
+package BreakCamelCase;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-class CamelCaseTest {
-  
+
+public class CamelCaseTest {
+
+    @Test
+    public void tests() {
+        assertEquals( "Incorrect", "camel Casing", CamelCase.breakCamelCase("camelCasing"));
+        assertEquals( "Incorrect", "camel Casing Test", CamelCase.breakCamelCase("camelCasingTest"));
+        assertEquals( "Incorrect", "camelcasingtest", CamelCase.breakCamelCase("camelcasingtest"));
+    }
+
+    @Test
+    public void shouldReturnBreakSentence(){
+
+        //Given
+        String sentence = "CamelCasing";
+
+        //When
+        String result = CamelCase.breakCamelCase(sentence);
+
+        //When
+        assertThat(result).isEqualTo("Camel Casing");
+
+    }
+
 }

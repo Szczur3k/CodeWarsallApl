8b47de2ca7c5340749209e7735a9f9b7f046919f
Szczur3k <mateuszlukaszczyk13@gmail.com>
2021-03-24 08:28:21 +0100

    deleted comments

--- src/main/java/EasyWallpaper/Wallpaper.java
+++ src/main/java/EasyWallpaper/Wallpaper.java
@@ -1,7 +1,5 @@
 package EasyWallpaper;
 
-import jdk.dynalink.beans.StaticClass;
-
 public class Wallpaper {
 
     static String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
@@ -26,19 +24,3 @@ public class Wallpaper {
 
     }
 }
-
-/*
-John wants to decorate a room with wallpaper. He wants a fool-proof method for getting it right.
-John knows that the rectangular room has a length of l meters, a width of w meters, a height of h meters.
-The standard width of the rolls he wants to buy is 52 centimeters. The length of a roll is 10 meters.
-He bears in mind however, that it’s best to have an extra length of wallpaper handy in case of mistakes or miscalculations
-so he wants to buy a length 15% greater than the one he needs.
-
-Rolka ma 52cm WIDTH
-Rolka ma 10metrow LENGTH
-
-
-Your function wallpaper(l, w, h) should return as a plain English word in lower case the number of rolls he must buy.
-
-
- */

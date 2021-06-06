ea884c6149f4c99e5095b6c55ad6e7c7a255511d
Szczur3k <Mateuszlukaszczyk13@gmail.com>
2021-06-06 16:37:32 +0200

    Added Fibonacci with recursion

--- src/main/java/RecruitmentTasks/Fibonacci.java
+++ src/main/java/RecruitmentTasks/Fibonacci.java
@@ -2,16 +2,24 @@ package RecruitmentTasks;
 
 public class Fibonacci {
 
-    private static void printFibonacci() {
+    public static void printFibonacci() {
         int actualNum = 1;
         int numBefore = 0;
-        int tempNum = 0;
+        int tempNum;
         for (int i = 0; i < 100; i++) {
             tempNum = actualNum + numBefore;
             numBefore = actualNum;
             actualNum = tempNum;
             System.out.println(actualNum);
         }
+    }
+
+    public static int printFibonacciRecursion(int number) {
+
+        if (number == 1 || number == 2){
+            return 1;
+        }
+            return printFibonacciRecursion(number - 1) + printFibonacciRecursion(number - 2);
     }
 
 }
